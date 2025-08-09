9c042ea037b283e7f570de46954e47d39547c0c7
Tyler Nguyen <tylerminhnguyen@yahoo.com>
2025-08-09 11:30:32 -0400

    updated all CRUD ops for tasks

--- backend/controllers/goalControllers/read.js
+++ backend/controllers/goalControllers/read.js
@@ -9,14 +9,14 @@ exports.readGoalById = async (req, res) => {
             return res.status(400).json({ error: 'Invalid goal ID format' });
         }
         // Find goal by ID from request parameters
-        const goalId = await Goal.findById(req.params.id);
+        const goal = await Goal.findById(req.params.id);
         // if goal not found, return 404
-        if (!goalId) {
+        if (!goal) {
             return res.status(404).json({ error: 'Goal not found' });
         }
 
-        res.status(200).json(goalId); // Respond with the found goal and a 200 status code
-        console.log('Goal found successfully: ', goalId);
+        res.status(200).json(goal); // Respond with the found goal and a 200 status code
+        console.log('Goal found successfully: ', goal);
     } catch (error) {
         console.error('Error finding goal by ID: ', error);
         res.status(500).json({ error: 'Internal server error' });
--- backend/controllers/taskControllers/delete.js
+++ backend/controllers/taskControllers/delete.js
@@ -1,1 +1,24 @@
 // Delete operation function for task
+const { Task } = require('../../model/dbSchema');
+const mongoose= require('mongoose');
+
+
+exports.deleteTask = async(req, res) =>{
+    try{
+        const taskId = req.params.id; // Get task ID from request parameters
+
+        // Check if the task ID is valid
+        if (!mongoose.Types.ObjectId.isValid(taskId)) {
+            return res.status(400).json({ error: 'Invalid task ID format' });
+        }
+
+        const deleteTask = await Task.findByIdAndDelete(taskId); // Delete task by Id
+        // If task not found, return 404
+        if (!deleteTask) {
+            return res.status(404).json({ error: 'Task not found' });
+        }
+    } catch (error) {
+        console.error('Error deleting task: ', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
